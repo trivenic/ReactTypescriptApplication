07e1d8550d4e1ab08439341c75ae6b6bb2fa8772
Triveni Pratik Patel (Chaudhari) <trivenic@cybage.com>
2021-08-23 16:34:57 +0530

    tsconfig.json added

---
+++ src/component/hello/Hello.tsx
@@ -0,0 +1,8 @@
+// export default const Hello =()=>{
+//     <h1>This is typescript Application</h1>
+// }
+
+function Hello({name}:{name:string}){
+    return <h1>Welcome {name}</h1>
+}
+export default Hello;
